9a161ef8484690739165b4541b64a94a825aa546
honestus <onesto.giuseppe@gmail.com>
2025-09-03 21:47:25 +0200

    Changes the llm-agent prompt structure in order to handle the possibility to run multiple operations at a time when user asks multiple requests in the same message

--- llm_agent.py
+++ llm_agent.py
@@ -16,12 +16,13 @@ f"If the request is strictly associated with a method: validate the needed param
 To validate datetime objects, use datetime.datetime to refer to datetime class. For all the relative datetimes (e.g. tomorrow, next week, yesterday) validate using datetime.datetime.now() as baseline, and using timedelta.\n\
 When user asks for a whole day or date, use start_time=datetime.datetime.combine(whole_date, datetime.time.min), end_time=datetime.datetime.combine(whole_date, datetime.time.max) .\n\
 Include the corresponding method as {METHOD_ATTRIBUTE}, the validated parameters as {PARAMS_ATTRIBUTE}; for any missing parameters, include them as {MISSING_PARAMS_ATTRIBUTE}.\n\
+If the user asks for multiple atomic operations at once, validate each of them separately and add them in the 'requests' list on the response.\n\
 Generate a friendly reply message to answer the user request in the same language of the user, it must only include friendly natural language words, and must never include any reference to code. Include the most important details of the user request, i.e. which parameters are used to satisfy his request. Comunicate them in very user friendly language.\n\
 When you are successfully validating method and parameters (no missing parameters), the reply message must only confirm you are actually working on the request. \n' \
 If the request is not associated with any method: set {METHOD_ATTRIBUTE} as '', {PARAMS_ATTRIBUTE} as dict(), {MISSING_PARAMS_ATTRIBUTE} as [], and try answering with the other info you have, for example price/service/duration.\n\
 You must always validate the corresponding Python 'method' and 'params' to the user request, don't ever answer based on previous conversation.\n"
 
-end_of_prompt = f"Dont generate any code. Return the output very short as:  __start__{{ {METHOD_ATTRIBUTE!r}: method_name, {PARAMS_ATTRIBUTE!r}: {{param_name: param_value}}, 'reply_to_user': reply_message, {MISSING_PARAMS_ATTRIBUTE!r}: [param_name], 'user_language': lang}}__end__"
+end_of_prompt = f"Dont generate any code. Return the output very short as:  __start__{{{REQUEST_ATTRIBUTE}: [{{{METHOD_ATTRIBUTE!r}: method_name, {PARAMS_ATTRIBUTE!r}: {{param_name: param_value}}, {MISSING_PARAMS_ATTRIBUTE!r}: [param_name]}},] , 'reply_to_user': reply_message, 'user_language': lang }}__end__"
 
 
 output_to_user_prompt = f"You are a barber reservation assistant. Your role is System. \n\
@@ -90,16 +91,16 @@ def validate_llm_model(model):
 
 
 class NLUAgent:
-    def __init__(self, business_agent, gemini_key, huggingface_key):
+    def __init__(self, business_agent, gemini_key, huggingface_key, huggingface_model="meta-llama/llama-3.1-8b-instruct"):
         self.business_agent = business_agent
         
         #self.prompt = prompt
         #self.prompt_output_requested_str = end_of_prompt
         self.past_conversation_messages = defaultdict(list)
-        self.last_llm_validated_response = {}
+        self.last_user_structured_requests = {}
 
         self.__init_llm__(model='gemini', api_key=gemini_key)
-        self.__init_llm__(model='huggingface', api_key=huggingface_key)
+        self.__init_llm__(model='huggingface', api_key=huggingface_key, model_name=huggingface_model)
         
         self.status = NO_PREVIOUS_CONVERSATION_STATUS            
 
@@ -112,7 +113,7 @@ class NLUAgent:
         
         llm_prompt = self.__build_prompt_message__(user=user, user_message=user_message)
         print('Sending to llm:', llm_prompt)
-        #return
+      
         try:
             llm_response = self.query_llm(message=llm_prompt, model='gemini', map_to_dct=True)
         except:
@@ -125,48 +126,50 @@ class NLUAgent:
             self.past_conversation_messages[user].append(('User', user_message))
 
         llm_response = self.__validate_llm_request_params__(llm_response)
-        print('LLM Response user:', llm_response.get(PARAMS_ATTRIBUTE).get('user'))
-
-        llm_response[USER_ATTRIBUTE] = user if user!='system' else ''
-        if llm_response.get(PARAMS_ATTRIBUTE, {}).get('user') in ['admin','system']: ##should never happen as far as we don't expose 'user' param in methods written in the prompt
-            llm_response[PARAMS_ATTRIBUTE].pop('user') ##removing 'user' from request parameters in order to avoid any potential not-allowed operations
-        print('LLM Response:', llm_response)
-        self.update_status(llm_response=llm_response)
-        print('Status:', self.status)
+        print('LLM Response:', llm_response)        
+                
         message_to_user = llm_response.get(REPLY_ATTRIBUTE)
-        validated_method_response = {k:v for k,v in llm_response.items() if k in [METHOD_ATTRIBUTE, PARAMS_ATTRIBUTE, ]}
-        print('Msg to user:' , message_to_user)
+
         self.send_message_to_user(message_to_user)
         self.past_conversation_messages[user].append(('System', message_to_user))
-        self.last_llm_validated_response[user]=validated_method_response
+        self.last_user_structured_requests[user]=llm_response[REQUEST_ATTRIBUTE]
+        
+        user_requests_outputs = []
+        for user_request in llm_response.get(REQUEST_ATTRIBUTE, []):
+            self.update_status(structured_request=user_request)
 
-        if self.status == WAITING_FOR_BOOKING_AGENT_STATUS:
-            print('Making action with params...', llm_response)
-            b_a_request, b_a_success, b_a_response_msg, b_a_extra_msg = self.send_request_to_business_agent(request=llm_response)
-            if b_a_success:
-                self.update_status(status=VALID_ACTION_PERFORMED_STATUS)
-                self.last_llm_validated_response.pop(user)
+            if self.status == WAITING_FOR_BOOKING_AGENT_STATUS:
+                user_request[USER_ATTRIBUTE] = user if user!='system' else '' ##setting 'user' to perform action on the system
+                if user_request.get(PARAMS_ATTRIBUTE, {}).get('user') in ['admin','system']: ##should never happen as far as we don't expose 'user' param in methods written in the prompt
+                    user_request[PARAMS_ATTRIBUTE].pop('user') ##removing user from request parameters if 'admin' or 'system' in order to avoid any potential not-allowed operations
+                print('Making action with params...', user_request)
+                sys_output = self.send_request_to_business_agent(request=user_request)
+                user_requests_outputs.append(sys_output)
+                if (sys_success:=sys_output[1]):
+                    self.update_status(status=VALID_ACTION_PERFORMED_STATUS)
+                    #self.last_user_structured_requests.pop(user)
 
-            user_prev_conversation = self.get_user_past_conversation(user)
-            last_user_msg_index = -([role for role,msg in user_prev_conversation][::-1].index('User')) 
-            prev_conversation_to_include = user_prev_conversation[last_user_msg_index-1:]
-            prompt_to_user_friendly = output_to_user_prompt.format(previous_conversation=self.render_user_past_conversation_messages(prev_conversation_to_include),
-                                                                 system_request=b_a_request, system_output_bool=b_a_success,
-                                                                 system_output=b_a_response_msg, extra=b_a_extra_msg,
-                                                                 user_language=llm_response.get('user_language','')
-                                                                 )
-            print('Sending to chat assistant...', prompt_to_user_friendly)
-            try:
-                friendly_response = self.query_llm(message=prompt_to_user_friendly, model='huggingface', map_to_dct=False)
-            except:
-                warnings.warn('huggingface fail... now gemini')
-                friendly_response = self.query_llm(message=prompt_to_user_friendly, model='gemini', map_to_dct=False)
+        user_prev_conversation = self.get_user_past_conversation(user)
+        last_user_msg_index = -([role for role,msg in user_prev_conversation][::-1].index('User')) 
+        prev_conversation_to_include = user_prev_conversation[last_user_msg_index-1:]
+        sys_request, sys_success, sys_response_msg, sys_extra_msg = zip(*user_requests_outputs)
+        prompt_to_user_friendly = output_to_user_prompt.format(previous_conversation=self.render_user_past_conversation_messages(prev_conversation_to_include),
+                                                             system_request=sys_request, system_output_bool=sys_success,
+                                                             system_output=sys_response_msg, extra=sys_extra_msg,
+                                                             user_language=llm_response.get('user_language','')
+                                                             )
+        print('Sending to chat assistant...', prompt_to_user_friendly)
+        try:
+            friendly_response = self.query_llm(message=prompt_to_user_friendly, model='huggingface', map_to_dct=False)
+        except:
+            warnings.warn('huggingface fail... now gemini')
+            friendly_response = self.query_llm(message=prompt_to_user_friendly, model='gemini', map_to_dct=False)
 
-            self.past_conversation_messages[user].append(('System', friendly_response))
-            print('Msg to user:' , friendly_response)
-            self.send_message_to_user(friendly_response)
+        self.past_conversation_messages[user].append(('System', friendly_response))
+        print('Msg to user:', friendly_response)
+        self.send_message_to_user(friendly_response)
         if self.status != VALID_ACTION_PERFORMED_STATUS:
-            self.update_status(WAITING_FOR_USER_REPLY_STATUS)
+            self.update_status(status=WAITING_FOR_USER_REPLY_STATUS)
 
         
         
@@ -190,7 +193,7 @@ class NLUAgent:
         available_methods = self.business_agent.get_exposed_methods_params(user, as_string=True)
         formatted_available_methods = '- ' + ';\n- '.join(available_methods)
 
-        last_request_method = self.last_llm_validated_response.get(user)
+        last_request_method = self.last_user_structured_requests.get(user)
         
         return prompt.format(services=formatted_available_services, 
                                   opening_hours=formatted_opening_hours, 
@@ -201,17 +204,17 @@ class NLUAgent:
             f"\n --- User:  {user_message} ---\n"+ \
             end_of_prompt
 
-    def update_status(self, status=None, llm_response=None):
+    def update_status(self, status=None, structured_request=None):
         if status is not None:
             self.status = status
             return
-        if not llm_response.get(METHOD_ATTRIBUTE):
+        if not structured_request.get(METHOD_ATTRIBUTE):
             self.status = INVALID_REQUEST_STATUS
             return
-        if llm_response.get(MISSING_PARAMS_ATTRIBUTE):
+        if structured_request.get(MISSING_PARAMS_ATTRIBUTE):
             self.status = MISSING_PARAMETERS_STATUS
             return
-        if llm_response.get(METHOD_ATTRIBUTE) and llm_response.get(PARAMS_ATTRIBUTE) and not llm_response.get(MISSING_PARAMS_ATTRIBUTE):
+        if structured_request.get(METHOD_ATTRIBUTE) and structured_request.get(PARAMS_ATTRIBUTE) and not structured_request.get(MISSING_PARAMS_ATTRIBUTE):
             self.status = WAITING_FOR_BOOKING_AGENT_STATUS
             return
         self.status = ERROR_STATUS
@@ -222,7 +225,7 @@ class NLUAgent:
         if not map_to_dct:
             return llm_response
         try:
-            llm_response = llm_response.strip().split('__start__')[-1].split('__end__')[0]
+            llm_response = llm_response.strip().split('__start__')[-1].split('__end__')[0].strip()
             llm_response_dict = _safe_eval_dict(llm_response)
             return llm_response_dict
         except NameError as e:
@@ -247,11 +250,23 @@ class NLUAgent:
     def preprocess_user_message(self, message):
         return message
 
-    def __init_llm__(self, model, api_key):
+    def __init_llm__(self, model, api_key, model_name=None):
+        """
+        model must be either 'gemini' or 'huggingface'
+        api_key must be the corresponding api_key (i.e. gemini api_key or huggingface_hub api_key)
+        model_name is the corresponding huggingface model, if None is provided: llama3.1-8b will be the used by default.
+        Notice that model_name is ignored if model!='huggingface'
+        """
         if getattr(self, 'llm', None) is None:
             self.llm = {}
         model = validate_llm_model(model)
-        self.llm[model] = InferenceClient(api_key=api_key) if model=='huggingface' else genai.Client(api_key=api_key)
+        if model=='gemini':
+            self.llm[model] = genai.Client(api_key=api_key)
+            return
+        if model=='huggingface':
+            self.llm[model] = InferenceClient(api_key=api_key, model=model_name if model_name else "meta-llama/llama-3.1-8b-instruct")
+            return
+            
 
     def __llm_response_to_str__(self, response, model):
         model = validate_llm_model(model)
@@ -264,36 +279,39 @@ class NLUAgent:
             return self.llm['gemini'].models.generate_content(
                 model="gemini-2.5-flash",
                 contents=message )
-        
-        return self.llm['huggingface'].chat.completions.create(
-                  model="meta-llama/llama-3.1-8b-instruct",
-                  messages=[
-                    {
-                      "role": "user",
-                      "content": message
-                    }
-                  ]
-            )
+        try:
+            msg_dct = {"role": "user", "content": message}
+            response = self.llm['huggingface'].chat.completions.create(
+                    messages=[msg_dct],
+                   )
+        except:
+            msg_dct["model"] = 'meta-llama/llama-3.1-8b-instruct'
+            response = self.llm['huggingface'].chat.completions.create(
+                    messages=[msg_dct],
+               )
+        return response
+            
 
             
     def __validate_llm_request_params__(self, request):
-        print('LLM raw request:', request)
-        request = {k.lower():v for k,v in request.items()}
-        if METHOD_ATTRIBUTE in request:
-            request[METHOD_ATTRIBUTE] = request[METHOD_ATTRIBUTE].lower() if request[METHOD_ATTRIBUTE] else ''
-        if PARAMS_ATTRIBUTE in request:
-            if not request[PARAMS_ATTRIBUTE]:
-                request[PARAMS_ATTRIBUTE] = {}
-            request[PARAMS_ATTRIBUTE] = {k.lower():v for k,v in request[PARAMS_ATTRIBUTE].items()}
-            for time_param in ['start_time', 'end_time', 'old_start_time', 'new_start_time']:
-                if time_param in request[PARAMS_ATTRIBUTE]:
-                    request[PARAMS_ATTRIBUTE][time_param] = map_datetime_to_next_slot_datetime(request[PARAMS_ATTRIBUTE][time_param])
-            for service_param in ['service_name', 'new_service_name', 'old_service_name']:
-                if service_param in request[PARAMS_ATTRIBUTE]:
-                    request[PARAMS_ATTRIBUTE][service_param] = request[PARAMS_ATTRIBUTE][service_param].lower()
-            for duration_param in ['minutes_duration', 'new_minutes_duration', 'old_minutes_duration']:
-                if duration_param in request[PARAMS_ATTRIBUTE]:
-                    request[PARAMS_ATTRIBUTE][duration_param] = int(request[PARAMS_ATTRIBUTE][duration_param])
+        request = {str(k).lower():v for k,v in request.items()}
+        request_list_dct = request.get(REQUEST_ATTRIBUTE, [])
+        for single_request in request_list_dct:
+            if METHOD_ATTRIBUTE in single_request and bool(single_request[METHOD_ATTRIBUTE]):
+                single_request[METHOD_ATTRIBUTE] = str(single_request[METHOD_ATTRIBUTE]).lower() if single_request[METHOD_ATTRIBUTE] else ''
+            if PARAMS_ATTRIBUTE in single_request:
+                if not single_request[PARAMS_ATTRIBUTE]:
+                    single_request[PARAMS_ATTRIBUTE] = {}
+                single_request[PARAMS_ATTRIBUTE] = {k.lower():v for k,v in single_request[PARAMS_ATTRIBUTE].items()}
+                for time_param in ['start_time', 'end_time', 'old_start_time', 'new_start_time']:
+                    if time_param in single_request[PARAMS_ATTRIBUTE]:
+                        single_request[PARAMS_ATTRIBUTE][time_param] = map_datetime_to_next_slot_datetime(single_request[PARAMS_ATTRIBUTE][time_param])
+                for service_param in ['service_name', 'new_service_name', 'old_service_name']:
+                    if service_param in single_request[PARAMS_ATTRIBUTE]:
+                        single_request[PARAMS_ATTRIBUTE][service_param] = single_request[PARAMS_ATTRIBUTE][service_param].lower()
+                for duration_param in ['minutes_duration', 'new_minutes_duration', 'old_minutes_duration']:
+                    if duration_param in single_request[PARAMS_ATTRIBUTE]:
+                        single_request[PARAMS_ATTRIBUTE][duration_param] = int(single_request[PARAMS_ATTRIBUTE][duration_param])
         return request
 
 
